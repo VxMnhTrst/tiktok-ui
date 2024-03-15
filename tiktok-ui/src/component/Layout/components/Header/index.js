d60868d4781c26821079ea8067006ce957ec8fba
TheTriesteCLC <148186018+TheTriesteCLC@users.noreply.github.com>
2024-03-15 14:25:09 +0700

    Done search bar

--- tiktok-ui/src/component/Layout/components/Header/index.js
+++ tiktok-ui/src/component/Layout/components/Header/index.js
@@ -1,14 +1,32 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
+import images from '../../../../assets/images';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <h1>Logo</h1>
-            <h1>Searchbox</h1>
-            <h1>Login/Logout</h1>
+            <img src={images.logo} alt="Tiktok logo"></img>
+
+            <div className={cx('search-bar')}>
+                <form>
+                    <input type="text" placeholder="Tìm kiếm đê..."></input>
+                    <button className={cx('clear-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark}> </FontAwesomeIcon>
+                    </button>
+                    <button className={cx('loading-btn')}>
+                        <FontAwesomeIcon icon={faSpinner}> </FontAwesomeIcon>
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass}> </FontAwesomeIcon>
+                    </button>
+                </form>
+            </div>
+            <div className={cx('actions')}></div>
         </header>
     );
 }
